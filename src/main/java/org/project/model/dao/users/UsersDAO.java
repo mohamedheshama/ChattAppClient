89bd5a0885632e5a908b717dacedfa06261af82c
ImanFouad96 <ifselim77@gmail.com>
2020-02-10 15:40:18 +0200

    users and usersDAO modification

--- src/main/java/org/project/model/dao/users/UsersDAO.java
+++ src/main/java/org/project/model/dao/users/UsersDAO.java
@@ -1,19 +1,21 @@
 package org.project.model.dao.users;
 
 import javafx.collections.ObservableList;
-import org.project.model.dao.friends.Friends;
+//import org.project.model.dao.friends.Friends;
+
+import java.util.ArrayList;
 
 public interface UsersDAO {
 
-    Users login(String phone_number);
+    Users login(String phone_number,String password);
 
     boolean register(Users user);
 
     boolean updateUser(Users user);
 
-    ObservableList<Friends> getUserFriends(Users user);
+    ArrayList<Users>getUserFriends(Users user);
 
-    ObservableList<Friends> getUserNotifications(Users user);
+    ArrayList<Users>getUserNotifications(Users user);
 
     boolean isUserExist(String phone_number);
 
