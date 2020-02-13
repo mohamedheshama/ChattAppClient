23dde8bfefab7c02d829cb6aa6963555c15c209c
ImanFouad96 <ifselim77@gmail.com>
2020-02-13 17:30:43 +0200

    Adding ClientInterface

---
+++ src/main/java/org/project/controller/ClientInterface.java
@@ -0,0 +1,12 @@
+package org.project.controller;
+
+import org.project.model.dao.users.UserStatus;
+import org.project.model.dao.users.Users;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ClientInterface extends Remote {
+    public Users getUser () throws RemoteException;
+    public void recieveUpdateStatus(UserStatus status,int id) throws RemoteException;
+}
