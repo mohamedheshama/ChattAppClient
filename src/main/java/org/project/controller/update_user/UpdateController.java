a9aadbd3ebb3c10c138501782d95c0c7be8827b6
hendelaraby <hend3229@gmail.com>
2020-02-13 16:55:08 +0200

    connect RegisterController with server

---
+++ src/main/java/org/project/controller/update_user/UpdateController.java
@@ -0,0 +1,15 @@
+package org.project.controller.update_user;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UpdateController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/org/project/controller/update_user/UpdateInterface.java
@@ -0,0 +1,4 @@
+package org.project.controller.update_user;
+
+public interface UpdateInterface {
+}
