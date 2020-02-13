159ecad95ac28f1a0279861ff8777753d34ff014
shimaa1091993 <shimaamohamed1091993@gmail.com>
2020-02-13 17:24:33 +0200

    MainChatController

--- src/main/java/org/project/controller/chat_home/right_side/MainChatController.java
+++ src/main/java/org/project/controller/chat_home/right_side/MainChatController.java
@@ -1,57 +1,197 @@
 package org.project.controller.chat_home.right_side;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXColorPicker;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXToggleButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.control.ListView;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.web.HTMLEditor;
-import javafx.scene.web.WebView;
-import org.project.controller.chat_home.ChatListener;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import org.project.controller.chat_home.HomeController;
 import org.project.controller.messages.Message;
 import org.project.controller.messages.MessageType;
+import org.project.model.ChatRoom;
+import org.project.model.dao.users.Users;
 
-import java.util.logging.Logger;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class MainChatController {
+public class MainChatController implements Initializable {
+    //todo initializ these variables from HomeContrroller
+    Users mUser;
+    HomeController homeController;
+    ChatRoom chatRoom = new ChatRoom();
     @FXML
-    HTMLEditor messageBox;
+    private JFXButton sendMsgImgBtn;
     @FXML
-    ListView chatPane;
-    Logger logger = Logger.getGlobal();
+    private TextArea msgTxtField;
+    @FXML
+    private VBox showMsgsBox;
+    @FXML
+    private ImageView attachFileImgBtn;
+    @FXML
+    private JFXButton addGroupMembersImgBtn;
+    @FXML
+    private Label chatReceiversTxtLabel;
+    @FXML
+    private JFXButton saveChatImgBtn;
+    @FXML
+    private JFXToggleButton italicButton;
+    @FXML
+    private JFXToggleButton boldButton;
+    @FXML
+    private JFXComboBox sizeComboBox;
+    @FXML
+    private JFXComboBox fontComboBox;
+    @FXML
+    private JFXColorPicker fontColorPicker;
+    private String colorPicked;
+    private String fontFamily = "Arial";
+    private int sizePicked;
+    private boolean italic;
+    private boolean bold;
 
-    public void sendMethod(KeyEvent keyEvent) {
+    public ImageView getAttachFileImgBtn() {
+        return attachFileImgBtn;
     }
 
-    public void sendButtonAction(ActionEvent actionEvent) {
-        String msg = messageBox.getHtmlText();
-        ChatListener.send(msg);
-        //todo expected that msg is sent succefuly and listner called the addTochat function
-        Message recieved = new Message();
-        recieved.setType(MessageType.USER);
-        recieved.setMsg(msg);
-        addToChat(recieved);
-        System.out.println("done Adding the message");
-        chatPane.setPrefSize(60, 60);
-        // chatPane.getItems().add("hihihihihihi");
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        chatReceiversTxtLabel.setText("myFrined");
+        msgTxtField.setWrapText(true);
+        msgTxtField.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                sendMsgToHomeController();
+            }
+        });
+        fontComboBox.getItems().addAll(Font.getFontNames());
+        fontColorPicker.setValue(Color.BLACK);
+        colorPicked = toRGBCode(fontColorPicker.getValue());
+        fontColorPicker.valueProperty().addListener((obs, oldVal, newVal) -> {
+                    Color color = (Color) newVal;
+                    System.out.println("Color : " + toRGBCode(color));
+                    colorPicked = toRGBCode(color);
+                    setTextFieldStyle();
+                }
+        );
 
+        sizeComboBox.getItems().addAll(IntStream.rangeClosed(8, 28).boxed().collect(Collectors.toList()));
+        sizeComboBox.setValue(14);
+        sizePicked = 14;
+        sizeComboBox.valueProperty().addListener((obs, oldVal, newVal) -> {
+                    //Country current = (Country) newVal;
+                    int size = (int) newVal;
+                    System.out.println("Size : " + size);
+                    sizePicked = size;
+                    setTextFieldStyle();
+                }
+        );
+        fontComboBox.setValue(fontFamily);
+        fontComboBox.valueProperty().addListener((observableValue, o, t1) -> {
+            String fontName = t1.toString();
+            System.out.println(fontName);
+            this.fontFamily = fontName;
+            setTextFieldStyle();
+        });
+        boldButton.setOnAction((ActionEvent e) -> {
+            if (bold) {
+                bold = false;
+            } else {
+                bold = true;
+            }
+            System.out.println("Now bold is " + bold);
+            setTextFieldStyle();
+        });
+        italicButton.setOnAction((ActionEvent e) -> {
+            if (italic) {
+                italic = false;
+            } else {
+                italic = true;
+            }
+            System.out.println("Now italic is " + italic);
+            setTextFieldStyle();
+        });
+        setTextFieldStyle();
     }
 
-
-    public void addToChat(Message msg) {
-        logger.warning(msg.getMsg());
-//        Image image = new Image(getClass().getClassLoader().getResource("../resources/org/project/images/bg2.jpg").toString());
-        //      ImageView profileImage = new ImageView(image);
-        WebView webView = new WebView();
-        webView.getEngine().loadContent(msg.getMsg());
-        webView.setMaxWidth(300);
-        HBox hBox = new HBox();
-        hBox.setMaxWidth(chatPane.getWidth() - 20);
-        hBox.setAlignment(Pos.TOP_RIGHT);
-        hBox.getChildren().addAll(webView);
-        chatPane.getItems().add(hBox);
-
+    public String toRGBCode(Color color) {
+        return String.format("#%02X%02X%02X",
+                (int) (color.getRed() * 255),
+                (int) (color.getGreen() * 255),
+                (int) (color.getBlue() * 255));
     }
 
+    public FontWeight getFontWeight() {
+        if (bold) {
+            return FontWeight.BOLD;
+        } else {
+            return FontWeight.LIGHT;
+        }
+    }
+
+    public FontPosture getFontPosture() {
+        if (italic) {
+            return FontPosture.ITALIC;
+        } else {
+            return FontPosture.REGULAR;
+        }
+    }
+
+    public void setTextFieldStyle() {
+        System.out.println("s;geod");
+        String str = msgTxtField.getText().toString();
+        msgTxtField.setText("");
+        msgTxtField.setStyle("-fx-font-family: \"" + fontFamily + "\"; " + "-fx-text-fill: " + colorPicked + ";" + "-fx-font-size: " + sizePicked + ";" + " -fx-font-weight:" + getFontWeight().name() + ";" + " -fx-font-style:" + getFontPosture().name());
+        msgTxtField.setText(str);
+    }
+
+    public ChatRoom getChatRoom() {
+        return chatRoom;
+    }
+
+    public void setChatRoom(ChatRoom chatRoom) {
+        this.chatRoom = chatRoom;
+    }
+
+    public void sendMessage(ActionEvent actionEvent) {
+        sendMsgToHomeController();
+    }
+
+    private void sendMsgToHomeController() {
+        Message newMsg = new Message();
+        newMsg.setMsg(msgTxtField.getText());
+        newMsg.setType(MessageType.USER);
+        newMsg.setFontFamily(fontFamily);
+        newMsg.setTextFill(colorPicked);
+        newMsg.setFontSize(sizePicked);
+        newMsg.setUser(mUser);
+        newMsg.setFontWeight(getFontWeight().name());
+        homeController.sendMsg(newMsg, chatRoom);
+        msgTxtField.setText("");
+    }
+
+    public void displayMessagesFromArrList() {
+        for (Message message : chatRoom.getChatRoomMessage()) {
+            if (message.getUser().getId() == mUser.getId()) {
+                // todo set allignment to right and displayMsg
+            } else {
+                // todo set alignment to left nad display message
+            }
+        }
+    }
+
+    private void displayMsg(Message msg /* todo here is the Alignment type*/) {
+        // todo display msg with specified alignment
+    }
 }
