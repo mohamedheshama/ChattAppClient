f175dccf5ad0ae269d3ba2427c1e6e13f620c42d
ImanFouad96 <ifselim77@gmail.com>
2020-02-15 01:28:08 +0200

    settings icon in user profile

--- src/main/java/org/project/controller/chat_home/left_side/UserIconController.java
+++ src/main/java/org/project/controller/chat_home/left_side/UserIconController.java
@@ -1,8 +1,13 @@
-package org.project.controller.chat_home.left_side;;
+package org.project.controller.chat_home.left_side;
 
+;
+
+import com.jfoenix.controls.JFXComboBox;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 
@@ -13,14 +18,28 @@ public class UserIconController implements Initializable {
 
     public ChoiceBox choicebox;
     public Circle Userimage;
+    public ImageView settings_icon;
+    public JFXComboBox settings;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Image userPicture= new Image(getClass().getResource("/org/project/images/iman.jpg").toExternalForm(),false);
+        Image userPicture = new Image(getClass().getResource("/org/project/images/iman.jpg").toExternalForm(), false);
         Userimage.setFill(new ImagePattern(userPicture));
-        choicebox.getItems().addAll("Online","Busy","Away");
+        choicebox.getItems().addAll("Online", "Busy", "Away");
         choicebox.setValue("Online");
+
+
+
+
+        settings_icon.setImage(new Image(getClass().getResource("/org/project/images/settings.png").toExternalForm()));
+        settings.getItems().addAll("Update Profile","Logout");
+
+
+
+
+
+
 
     }
 }
