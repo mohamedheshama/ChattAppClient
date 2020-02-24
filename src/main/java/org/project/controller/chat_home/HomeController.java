dea64f402d4d8160e063783def7a2f1455d83576
shimaa1091993 <shimaamohamed1091993@gmail.com>
2020-02-24 18:36:48 +0200

    making notification for announcement instead of custom dialog

--- src/main/java/org/project/controller/chat_home/HomeController.java
+++ src/main/java/org/project/controller/chat_home/HomeController.java
@@ -1,17 +1,14 @@
 package org.project.controller.chat_home;
 
 import javafx.application.Platform;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -19,10 +16,11 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.util.Pair;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 import org.project.controller.ClientImp;
 import org.project.controller.MainDeligator;
 import org.project.controller.chat_home.left_side.LeftSideController;
@@ -34,6 +32,7 @@ import org.project.model.dao.users.Users;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
@@ -234,55 +233,72 @@ public class HomeController implements Initializable, Serializable {
 
     public void recieveMsgFromAdmin(Message newMsg, Users onlineUser) {
 
-        System.out.println("recieve message from admin in homecontroller" + " newMsg" + newMsg + "newMsgllll       " + newMsg + "  ////////////onlineUser" + onlineUser.getId());
-        System.out.println("user.getId()" + user.getId());
         if (!newMsg.getMsg().trim().equals("") && onlineUser.getId() == user.getId()) {
-            System.out.println("in the show Notification -> " + newMsg.getMsg());
             Thread thread = new Thread(() -> {
-                Dialog<Pair<String, String>> dialog = new Dialog<>();
-                dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
-                VBox vBox = new VBox();
-                vBox.setPadding(new Insets(20, 20, 20, 20));
-                Text text = new Text(newMsg.getMsg());
-                text.setStyle("-fx-font-family: \"" + newMsg.getFontFamily() + "\"; "
-                        + ";" + "-fx-font-size: " + newMsg.getFontSize()
-                        + ";" + " -fx-font-weight:" + newMsg.getFontWeight()
-                        + ";" + " -fx-font-style:" + newMsg.getFontPosture()
-                        + ";" + " -fx-fill: " + newMsg.getTextFill());
-                System.out.println("newMsg.getTextFill()" + newMsg.getTextFill());
-                if (newMsg.getMsg().trim().length() > 100) {
-                    text.setWrappingWidth(570);
+                VBox announcementVbox = drawAnnouncementVbox(newMsg);
+                Notifications notificationBuilder = Notifications.create()
+                        .title("Announcement")
+                        .graphic(announcementVbox)
+                        .hideAfter(Duration.seconds(60))
+                        .position(Pos.BOTTOM_RIGHT)
+                        .onAction(new EventHandler<ActionEvent>() {
+                            @Override
+                            public void handle(ActionEvent event) {
+                                System.out.println("announcement has been clicked");
+                            }
+                        });
+                notificationBuilder.darkStyle();
+                getStage().requestFocus();
+                AudioClip clip = null;
+                try {
+                    clip = new AudioClip(getClass().getResource("/org/project/sounds/notification.wav").toURI().toString());
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
                 }
-                ScrollPane scrollPane = new ScrollPane();
-               ImageView imageView = new ImageView();
-                imageView.setImage(new Image(getClass().getResource("/org/project/images/admin-icon.png").toExternalForm()));
-                imageView.setFitWidth(30);
-                imageView.setFitHeight(30);
-                HBox hBox =new HBox();
-                hBox.setPadding(new Insets(5,0,10,0));
-                hBox.getChildren().add(imageView);
-                hBox.setAlignment(Pos.CENTER);
-                HBox hBox1 =new HBox();
-                hBox1.setPadding(new Insets(10,10,10,10));
-                hBox1.getChildren().add(text);
-                scrollPane.setVmax(500);
-                scrollPane.setPrefSize(115, 150);
-                scrollPane.setFitToWidth(true);
-                scrollPane.setFitToHeight(true);
-                scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-                scrollPane.setContent(hBox1);
-                vBox.getChildren().add(hBox);
-
-                vBox.getChildren().add(scrollPane);
-                vBox.setStyle("-fx-background-color: aliceblue");
-                vBox.setPrefWidth(650);
-                vBox.setMaxWidth(650);
-                vBox.setMaxHeight(500);
-                dialog.getDialogPane().setContent(vBox);
-                dialog.initStyle(StageStyle.UTILITY);
-                dialog.showAndWait();
+                clip.play();
+                notificationBuilder.show();
             });
             Platform.runLater(thread);
         }
+    }
+
+    public VBox drawAnnouncementVbox(Message newMsg) {
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(20, 20, 20, 20));
+        Text text = new Text(newMsg.getMsg());
+        text.setStyle("-fx-font-family: " + newMsg.getFontFamily()
+                + ";" + "-fx-font-size: " + newMsg.getFontSize()
+                + ";" + " -fx-font-weight:" + newMsg.getFontWeight()
+                + ";" + " -fx-font-style:" + newMsg.getFontPosture()
+                + ";" + " -fx-fill: " + newMsg.getTextFill());
+        if (newMsg.getMsg().trim().length() > 100) {
+            text.setWrappingWidth(570);
+        }
+        ScrollPane scrollPane = new ScrollPane();
+        ImageView imageView = new ImageView();
+        imageView.setImage(new Image(getClass().getResource("/org/project/images/admin-icon.png").toExternalForm()));
+        imageView.setFitWidth(30);
+        imageView.setFitHeight(30);
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(5, 0, 10, 0));
+        hBox.getChildren().add(imageView);
+        hBox.setAlignment(Pos.CENTER);
+        HBox hBox1 = new HBox();
+        hBox1.setPadding(new Insets(10, 10, 10, 10));
+        hBox1.getChildren().add(text);
+        scrollPane.setVmax(500);
+        scrollPane.setPrefSize(115, 150);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setContent(hBox1);
+        vBox.getChildren().add(hBox);
+
+        vBox.getChildren().add(scrollPane);
+        vBox.setStyle("-fx-background-color: aliceblue");
+        vBox.setPrefWidth(650);
+        vBox.setMaxWidth(650);
+        vBox.setMaxHeight(500);
+        return vBox;
     }
 }
